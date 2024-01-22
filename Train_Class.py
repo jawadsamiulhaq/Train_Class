12fd7d71b39c1353a531f6f1b3c14afa73619c73
jawadsamiulhaq <jawad786sami@gmail.com>
2024-01-22 21:53:03 +0500

    message

--- Train_Class.py
+++ Train_Class.py
@@ -21,7 +21,7 @@ class Train():
         self.seats = self.seats + 1
         
         print(f"You Cancell YOur Ticket! {self.seats}")
-intercity = Train("Hazara ExpressPX",100,90)
+intercity = Train("Hazara Express",100,90)
 intercity.getStatus()
 intercity.bookTicket()
 intercity.getStatus()
