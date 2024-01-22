13eda032019d7adc4cb1c668b3445ad6f8dfe645
Jawad Sami Ul Haq <110405348+jawadsamiulhaq@users.noreply.github.com>
2024-01-22 21:59:03 +0500

    Update Train_Class.py

--- Train_Class.py
+++ Train_Class.py
@@ -8,7 +8,7 @@ class Train():
     def getStatus(self):
         print("--------------Train Info------------")
         print(f"Name Of Train Is {self.name}")
-        print(f"Total Number Of Seats Available In {self.name} Is {self.seats}")
+        print(f"Total Number Of Seatss Available In {self.name} Is {self.seats}")
     def bookTicket(self):
         if(self.seats>0):
             print("-----------------------------------")
